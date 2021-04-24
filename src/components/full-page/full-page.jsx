1ee072bbaef0e9a79428056e477f3f518bac2db7
mantuok <mantuok@gmail.com>
2021-04-24 20:17:02 +0300

    Pixel Perfect verified

--- src/components/full-page/full-page.jsx
+++ src/components/full-page/full-page.jsx
@@ -37,7 +37,7 @@ const FullPage = () => {
             <h1 className="promo__header">Лига Банк</h1>
             <p className="promo__description">Кредиты на любой случай</p>
             <Link className="promo__link" to="/page-not-found">Рассчитать кредит</Link>
-            <img className="promo__cards-image" src="../img/black-and-white-cards.png" alt="Пример карт банка" width="398" height="240" />
+            <img className="promo__cards-image" src="../img/black-and-white-cards.png" alt="Пример карт банка" width="444" height="286" />
           </div>
         </section>
         <section className="main__converter converter">
